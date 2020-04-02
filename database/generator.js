57b5d5ae2374e5977f092b2b137e83eda9ada8d3
Eric Lau <ericlaucmo@gmail.com>
2020-04-02 12:57:41 -0700

    Data generator function for MongoDB

---
+++ database/generator.js
@@ -0,0 +1,44 @@
+const nameModel = require('./mongo.js');
+const faker = require('faker');
+
+const upperLimit = 1000000;
+
+const generator = () => {
+  return new Promise((resolve) =>
+    setTimeout(() => {
+      resolve({ name: faker.name.firstName() });
+    }, 0)
+  );
+};
+
+async function seed() {
+  for (let i = 1; i <= upperLimit; i++) {
+    const set = await Promise.all([
+      generator(),
+      generator(),
+      generator(),
+      generator(),
+      generator(),
+      generator(),
+      generator(),
+      generator(),
+      generator(),
+      generator()
+    ]);
+
+    await nameModel
+      .insertMany(set)
+      .then((success) => {
+        if (i === upperLimit) {
+          const end = process.hrtime.bigint();
+          const elapsedTime = parseInt(end - start, 10) / 60000000000;
+          console.log(`-------------- Finish --------------`);
+          console.log(`${elapsedTime} minutes`);
+        }
+      })
+      .catch((err) => {});
+  }
+}
+
+const start = process.hrtime.bigint();
+seed();
---
+++ database/mongo.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/fakeData', { useNewUrlParser: true });
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => console.log('Mongoose-d!'));
+
+const nameSchema = mongoose.Schema({
+  name: String
+});
+
+const nameModel = mongoose.model('Names', nameSchema);
+
+module.exports = nameModel;
