887325f22d915ea477f40c293893c98c8dfd1b45
Eric Lau <ericlaucmo@gmail.com>
2020-04-10 18:41:17 -0700

    Adjusted query methods to speed up gender queries

--- routers/routerPostgres.js
+++ routers/routerPostgres.js
@@ -17,68 +17,78 @@ const filter = ["Men's%20Shoe", "Women's%20Shoe", 'Running', 'Run'];
 })();
 
 const querier = async (method, keyword) => {
-  var query;
+  var query, start, end;
+
   if (method === 0) {
+    query = {
+      text: `SELECT data FROM data WHERE data->'collections' ? $1 ORDER BY id DESC LIMIT 50;`,
+      values: [`${keyword}`]
+    };
+  } else if (method === 1) {
     query = {
       text: `SELECT data FROM data WHERE data->>'type' LIKE $1 ORDER BY id DESC LIMIT 50;`,
       values: [`%${keyword}%`]
     };
   } else {
     query = {
-      text: `SELECT data FROM data WHERE data->'collections' ? $1 ORDER BY id DESC LIMIT 50;`,
-      values: [`${keyword}`]
+      text: `SELECT data FROM data WHERE data->>'type' LIKE $1 AND data->>'type' NOT LIKE '%Run%' ORDER BY id DESC LIMIT 50;`,
+      values: [`${keyword}%`]
     };
   }
 
   query.rowMode = 'array';
 
+  // Timer start
+  start = process.hrtime.bigint();
+
   const data = await client.query(query);
 
-  return data.rows.flat();
-};
+  // Timer end
+  end = process.hrtime.bigint();
 
-// prettier-ignore
-router.get('/search/:keyword', async (ctx) => {
-  var keyword = ctx.request.url.substring(8);
-  var start, end;
-
-  if (filter.includes(keyword)) {
-    try {
-      keyword = keyword.split('%20').join(' ');
-
-      if (keyword === 'Running') { keyword = 'nning'; }
-
-      // Timer start
-      start = process.hrtime.bigint();
-
-      // Query and respond
-      ctx.body = await querier(0, keyword);
-
-      // Timer end
-      end = process.hrtime.bigint();
-
-      if (keyword === 'nning') { keyword = 'Running'; }
-    } catch (err) {
-      ctx.body = err;
-    }
-  } else {
-    try {
-      // Timer start
-      start = process.hrtime.bigint();
-
-      // Query and respond
-      ctx.body = await querier(1, keyword);
-
-      // Timer end
-      end = process.hrtime.bigint();
-    } catch (err) {
-      ctx.body = err;
-    }
+  if (keyword === 'nning') {
+    keyword = 'Running';
   }
 
   console.log(
     `${keyword} in: ${(parseInt(end - start, 10) / 1e6).toFixed(2)} ms`
   );
+
+  return data.rows.flat();
+};
+
+router.get('/search/:keyword', async (ctx) => {
+  var keyword = ctx.request.url.substring(8);
+
+  if (filter.includes(keyword)) {
+    try {
+      if (keyword.includes('%20')) {
+        keyword = keyword.split('%20');
+
+        // Query and respond
+        ctx.body = await querier(
+          2,
+          keyword[0].substring(0, keyword[0].length - 2)
+        );
+      } else {
+        if (keyword === 'Running') {
+          keyword = 'nning';
+        }
+
+        // Query and respond
+        ctx.body = await querier(1, keyword);
+      }
+    } catch (err) {
+      ctx.body = err;
+    }
+  } else {
+    try {
+      // Query and respond
+      ctx.body = await querier(0, keyword);
+    } catch (err) {
+      ctx.body = err;
+    }
+  }
 });
 
 module.exports = router;
