e190109387589341afe14a61ecfa86253b1affc1
Eric Lau <ericlaucmo@gmail.com>
2020-04-14 19:14:06 -0700

    Removed Koa middleware

--- server/index.js
+++ server/index.js
@@ -1,20 +1,10 @@
-// Needy NewRelic
-require('newrelic');
-
 // Dependencies
 const Koa = require('koa');
-const bodyParser = require('koa-body');
-const fs = require('fs');
-const path = require('path');
 
 // Declarations
 const app = new Koa();
 const port = 3000;
-
 const routes = require('../routers/router.js');
-
-// Middleware
-app.use(bodyParser());
 
 // Router
 app.use(routes.routes());
